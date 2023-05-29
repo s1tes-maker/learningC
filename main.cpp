7b7062f6e875bb51030947757d8f3a84f65cc5ab
s1tes-maker <s1tes-maker@ya.ru>
2023-05-29 19:01:36 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+﻿// MyCMake.cpp: определяет точку входа для приложения.
+//
+
+#include "inout.h"
+
+using namespace std;
+
+#include <algorithm>
+
+int main() {
+    //vector<int> vec{ 0, 1, 2, 3, 10, 5, 6, 7, 8, 9 };
+    vector<int> vec;
+    read(vec);
+    sort(vec.begin(), vec.end());
+    print(vec);
+    return 0;
+}
+
